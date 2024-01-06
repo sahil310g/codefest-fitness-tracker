2a9ca8a77ae08aae9e37f15f8989e2053aaa8e6d
Madhur1808 <77977225+Madhur1808@users.noreply.github.com>
2024-01-06 13:09:46 +0530

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import Login from "./pages/Login";
+import { Home } from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">app </div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
