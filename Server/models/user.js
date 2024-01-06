bb9283206680c41a2b5947e47bcd4b886e73f75e
Ayushman74 <asmgarg74@gmail.com>
2024-01-06 13:05:15 +0530

    added gitignore

--- Server/models/user.js
+++ Server/models/user.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   fullName: String,
   email: String,
-  Activitys: [],
+  activities: [],
   password: String,
 });
+
 
 module.exports = mongoose.model('User', userSchema);
