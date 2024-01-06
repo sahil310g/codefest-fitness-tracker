f50540cf5987859e2e27bcbd0a1631ce6c6e559c
Ayushman74 <asmgarg74@gmail.com>
2024-01-06 14:46:05 +0530

    server route update

--- Server/models/user.js
+++ Server/models/user.js
@@ -3,14 +3,22 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   fullName: String,
   email: String,
-  goal : [activitySchema],
+  userInfo :[userInfoSchema],
+  goals : [activitySchema],
   activities: [calanderSchenma], 
   password: String
 });
 
+const userInfoSchema = new mongoose.Schema({
+  gender: String,
+  age: Number,
+  height: Number,
+  weight: Number
+});
+
 const calanderSchenma = new mongoose.Schema({
   date: Date,
-  activity : [activitySchema]
+  dailyActivity: [activitySchema]
 });
 
 const activitySchema = new mongoose.Schema({
