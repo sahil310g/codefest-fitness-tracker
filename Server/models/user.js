c2f3e9016c50894b0c2f32acc76e011ba8bd553f
Ayushman74 <asmgarg74@gmail.com>
2024-01-06 15:35:23 +0530

    server error resolve

--- Server/models/user.js
+++ Server/models/user.js
@@ -1,24 +1,10 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  fullName: String,
-  email: String,
-  userInfo :[userInfoSchema],
-  goals : [activitySchema],
-  activities: [calanderSchenma], 
-  password: String
-});
-
 const userInfoSchema = new mongoose.Schema({
   gender: String,
   age: Number,
   height: Number,
   weight: Number
-});
-
-const calanderSchenma = new mongoose.Schema({
-  date: Date,
-  dailyActivity: [activitySchema]
 });
 
 const activitySchema = new mongoose.Schema({
@@ -28,6 +14,20 @@ const activitySchema = new mongoose.Schema({
   water : String,
   dancing : String,
   swimming : String
+});
+
+const calanderSchenma = new mongoose.Schema({
+  date: Date,
+  dailyActivity: [activitySchema]
+});
+
+const userSchema = new mongoose.Schema({
+  fullName: String,
+  email: String,
+  userInfo :[userInfoSchema],
+  goals : [activitySchema],
+  activities: [calanderSchenma], 
+  password: String
 });
 
 module.exports = mongoose.model('User', userSchema);
