6bd0933b14b6cd5cecb8f9e656134f943b815a37
Sahil <sahil310g@gmail.com>
2024-01-06 14:23:04 +0530

    Updated schemas

--- Server/models/user.js
+++ Server/models/user.js
@@ -3,9 +3,21 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   fullName: String,
   email: String,
-  activities: [],
-  password: String,
+  goal : [activitySchema],
+  activities: [calanderSchenma], 
+  password: String
 });
 
+const calanderSchenma = new mongoose.Schema({
+  date: Date,
+  activity : [activitySchema]
+});
+
+const activitySchema = new mongoose.Schema({
+  running : String,
+  walking : String,
+  calorie: String,
+  water : String
+});
 
 module.exports = mongoose.model('User', userSchema);
